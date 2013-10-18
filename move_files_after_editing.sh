c699373ed19812f9195da4261230438af0649b87
johndmulhausen <johndylan@google.com>
2013-10-17 18:01:07 -0700

    Make it a real "move" by removing directories after getting them in place

--- move_files_after_editing.sh
+++ move_files_after_editing.sh
@@ -10,3 +10,6 @@ cp -fr batcache/advanced-cache.php wordpress/wp-content/
 cp -fr batcache/batcache.php wordpress/wp-content/plugins/
 cp -fr wp-memcache/object-cache.php wordpress/wp-content/
 cp -fr appengine-wordpress-plugin/. wordpress/wp-content/plugins/
+rm -rf batcache/
+rm -rf wp-memcache/
+rm -rf appengine-wordpress-plugin/
