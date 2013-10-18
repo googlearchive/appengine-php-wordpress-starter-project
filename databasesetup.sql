72d87cf0ce64bbd70a11c5f836e7b5d213b80643
johndmulhausen <johndylan@google.com>
2013-10-18 02:09:00 -0700

    Update databasesetup.sql

--- databasesetup.sql
+++ databasesetup.sql
@@ -1,3 +1,3 @@
 CREATE DATABASE IF NOT EXISTS wordpress_db;
-CREATE USER 'wp_user'@'localhost' IDENTIFIED BY 'wp_password';
-GRANT ALL PRIVILEGES ON wordpress_db.* TO 'wp_user'@'localhost';
+CREATE USER 'root'@'localhost';
+GRANT ALL PRIVILEGES ON wordpress_db.* TO 'root'@'localhost';
