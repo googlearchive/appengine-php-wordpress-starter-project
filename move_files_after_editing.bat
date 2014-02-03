cf0a13eeba809a4e4530a1e586c53d54b7d097ba
Andrew Jessup <jessup@andrew1.roam.corp.google.com>
2014-02-03 14:07:03 -0800

    Remove .bat file (no longer necessary)

--- move_files_after_editing.bat
+++
@@ -1,8 +0,0 @@
-move wp-config.php wordpress/
-move batcache/advanced-cache.php wordpress/wp-content/
-move batcache/batcache.php wordpress/wp-content/plugins/
-move wp-memcache/object-cache.php wordpress/wp-content/
-xcopy /s appengine-wordpress-plugin wordpress/wp-content/plugins/
-rmdir /s/q batcache/
-rmdir /s/q wp-memcache/
-rmdir /s/q appengine-wordpress-plugin/
