b3305edad7ffd0f625aa21da0d157d050831a885
Andrew Jessup <jessup@andrew1.roam.corp.google.com>
2014-02-03 14:07:52 -0800

    Corrects spacing

--- wp-config.php
+++ wp-config.php
@@ -91,7 +91,7 @@
 
     /** Absolute path to the WordPress directory. */
     if ( !defined('ABSPATH') )
-	define('ABSPATH', dirname(__FILE__) . '/wordpress/');
+        define('ABSPATH', dirname(__FILE__) . '/wordpress/');
 
     /** Sets up WordPress vars and included files. */
     require_once(ABSPATH . 'wp-settings.php');
