234fd866bf5dad927c35295eccf0cd15d5d0eaaf
Daniel Acton <code@danielacton.com>
2014-06-02 14:52:58 +0200

    Update wp-config.php
    I struggled with this for a couple hours. I set the password for the App Engine config, while it wasn't needed. It wasn't clear that it wasn't needed, so I'm hoping that adding this comment will help others in this situation.

--- wp-config.php
+++ wp-config.php
@@ -23,6 +23,7 @@
 
     if (isset($_SERVER['SERVER_SOFTWARE']) && strpos($_SERVER['SERVER_SOFTWARE'],'Google App Engine') !== false) {
         /** Live environment Cloud SQL login and SITE_URL info */
+        /** Note that from App Engine, the password is not required, so leave it blank here */
         define('DB_HOST', ':/cloudsql/your-project-id:wordpress');
         define('DB_USER', 'root');
         define('DB_PASSWORD', '');
